63ac2a1c022da7ec8a0ec93299594294cddca04a
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-09-16 17:30:40 +0530

    swaping added

--- index.js
+++ index.js
@@ -7,10 +7,56 @@ function removeDuplicate(sentence) {
  let uniqueWords = new Set(words);
 return [...uniqueWords].join(" ");
 }
-
-
 console.log(removeDuplicate(str));
 
 
 
+//2. sort an array in ascending order without usingsort logic
+let arr=[2,3,5,7,4,1,6,9]
+let n=arr.length
+function sortArray(arr)
+{
 
+for (i=0;i<n;i++)
+    {
+        for(j=0;j<n-1;j++)
+        {
+            if(arr[j]>arr[j+1]){
+
+            let temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp
+
+            }
+        }
+    } 
+    
+    return arr;
+    
+    
+}
+
+console.log(sortArray(arr));
+
+
+
+
+// 3.swap two variables without using a thrd variable
+
+function swap(a,b){
+
+      console.log("Before swap: a =", a, ", b =", b);
+
+    //   a=a+b;
+    //   b=a-b;
+    //   a=a-b;
+
+    a=a*b;
+    b=a/b;
+    a=a/b;
+
+      console.log("After swap: a =", a, ", b =", b);
+
+}
+
+swap(10,20)
