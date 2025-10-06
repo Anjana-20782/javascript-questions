edfd42f1b9dcad11343503b77b7de2cec607ed38
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-10-06 10:12:22 +0530

    new contents added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //1. remove duplicate from an array
 
-let str = "my name is anjana my friend name kannettan";
+// let str = "my name is anjana my friend name kannettan";
 
-function removeDuplicate(sentence) {
-  let words = sentence.split(" ");
- let uniqueWords = new Set(words);
-return [...uniqueWords].join(" ");
-}
-console.log(removeDuplicate(str));
+// function removeDuplicate(sentence) {
+//   let words = sentence.split(" ");
+//  let uniqueWords = new Set(words);
+// return [...uniqueWords].join(" ");
+// }
+// console.log(removeDuplicate(str));
+
+
 
 
 
@@ -15,31 +17,33 @@ console.log(removeDuplicate(str));
 
 //2. sort an array in ascending order without usingsort logic
 
-let arr=[2,3,5,7,4,1,6,9]
-let n=arr.length
-function sortArray(arr)
-{
+// let arr=[2,3,5,7,4,1,6,9]
+// let n=arr.length
+// function sortArray(arr)
+// {
 
-for (i=0;i<n;i++)
-    {
-        for(j=0;j<n-1;j++)
-        {
-            if(arr[j]>arr[j+1]){
+// for (i=0;i<n;i++)
+//     {
+//         for(j=0;j<n-1;j++)
+//         {
+//             if(arr[j]>arr[j+1]){
 
-            let temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp
+//             let temp=arr[j];
+//             arr[j]=arr[j+1];
+//             arr[j+1]=temp
 
-            }
-        }
-    } 
+//             }
+//         }
+//     } 
     
-    return arr;
+//     return arr;
     
     
-}
+// }
 
-console.log(sortArray(arr));
+// console.log(sortArray(arr));
+
+
 
 
 
@@ -82,23 +86,26 @@ console.log(sortArray(arr));
 
 // 3.swap two variables without using a thrd variable
 
-function swap(a,b){
+// function swap(a,b){
 
-      console.log("Before swap: a =", a, ", b =", b);
+//       console.log("Before swap: a =", a, ", b =", b);
 
-      a=a+b;
-      b=a-b;
-      a=a-b;
+//       a=a+b;
+//       b=a-b;
+//       a=a-b;
 
-    // a=a*b;
-    // b=a/b;
-    // a=a/b;
+//     // a=a*b;
+//     // b=a/b;
+//     // a=a/b;
 
-      console.log("After swap: a =", a, ", b =", b);
+//       console.log("After swap: a =", a, ", b =", b);
 
-}
+// }
 
-swap(10,20)
+// swap(10,20)
+
+
+
 
 
 
@@ -108,16 +115,17 @@ swap(10,20)
 // Implement a function like Array.prototype.map().
 
 
-function myMap(arr, callback) {
-  let result = []; 
+// function myMap(arr, callback) {
+//   let result = []; 
 
-  for (let i = 0; i < arr.length; i++) {
+//   for (let i = 0; i < arr.length; i++) {
    
-    result.push(callback(arr[i], i, arr));
-  }
+//     result.push(callback(arr[i], i, arr));
+//   }
 
-  return result; 
-}
+//   return result; 
+// }
+
 
 
 
@@ -157,29 +165,55 @@ function myMap(arr, callback) {
 
 
 
+
 // count the frencency of characters in a string
 
-let result="pineapple"
+// let result="pineapple"
 
-function countCharFrequency(str) {
-  let freq = {};   
-  for (let i = 0; i < str.length; i++) {   
-    let char = str[i];   
+// function countCharFrequency(str) {
+//   let freq = {};   
+//   for (let i = 0; i < str.length; i++) {   
+//     let char = str[i];   
 
-    if (freq[char]) {        
+//     if (freq[char]) {        
       
-      freq[char]++;         
+//       freq[char]++;         
       
-    } else {
-      freq[char] = 1;       
+//     } else {
+//       freq[char] = 1;       
       
-    }
-  }
+//     }
+//   }
 
-  return freq; 
+//   return freq; 
   
 
-}
+// }
 
 
-console.log(countCharFrequency(result))
+// console.log(countCharFrequency(result))
+
+
+
+
+
+
+let arr=["apple","orange","grape","apple","orange","apple"]
+
+let count={}
+for(i=0;i<arr.length;i++){
+
+  let item=arr[i]
+  if(count[item]){
+
+    count[item] +=1
+  }
+  else{
+
+    count[item]=1
+  }
+} 
+
+console.log(count)
+
+
